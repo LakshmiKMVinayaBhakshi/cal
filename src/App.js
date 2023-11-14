c5838795d6832cde292bdcb801749dc1d85b6f40
LakshmiKMVinayaBhakshi <lakshmikm.h9@outlook.com>
2023-11-14 19:05:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
-import { useReducer } from 'react';
-import './Calculator.css'
+import React, { useState } from 'react';
+import './App.css';
 
-const ACTIONS = {
-  ADD_DIGIT:'add-digit',
-  CHOOSE_OPERATION:'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT:'delete-digit',
-  EVALUATE : 'evaluate'
-
-}
-function reducer(state,{type,payload}){
-switch(type){
-  case  ACTIONS.ADD_DIGIT:
-    return{
-      ...state,
-      currentOperand: `$(currentOperand || "")$(payload.digit)`
-    }
-}
-}
 function App() {
-  // const [state,dispatch] = useReducer(reducer,{})  => is the syntax of useReducer hook 
-  const[currentOperand, previousOperand, operation] = useReducer(reducer,{})
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
 
-  dispatch({type:ACTIONS.ADD_DIGIT, paload:{digit:1}})
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
   return (
-    <div className='calculator-grid'>
-    <div className='output'>
-    <div className='previous-operand'>{previousOperand}{operation}</div>
-    <div className='current-operand'>{currentOperand}</div>
-    </div> 
-    <button className='span-two'>AC</button>
-    <button>DEL</button>
-    <button>÷</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>x</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>+</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>-</button>
-    <button>.</button>
-    <button>0</button>
-    <button className='span-two'>=</button>
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <input type="text" value={input} readOnly />
+          <div className="result">{result}</div>
+        </div>
+        <div className="buttons">
+          <button onClick={() => handleClick('7')}>7</button>
+          <button onClick={() => handleClick('8')}>8</button>
+          <button onClick={() => handleClick('9')}>9</button>
+          <button onClick={() => handleClick('/')}>/</button>
 
+          <button onClick={() => handleClick('4')}>4</button>
+          <button onClick={() => handleClick('5')}>5</button>
+          <button onClick={() => handleClick('6')}>6</button>
+          <button onClick={() => handleClick('*')}>*</button>
 
-    </div> 
+          <button onClick={() => handleClick('1')}>1</button>
+          <button onClick={() => handleClick('2')}>2</button>
+          <button onClick={() => handleClick('3')}>3</button>
+          <button onClick={() => handleClick('-')}>-</button>
+
+          <button onClick={() => handleClick('0')}>0</button>
+          <button onClick={() => handleClick('.')}>.</button>
+          <button onClick={() => handleClick('=')}>=</button>
+          <button onClick={() => handleClick('+')}>+</button>
+
+          <button onClick={() => handleClick('C')} className="clear">
+            C
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
