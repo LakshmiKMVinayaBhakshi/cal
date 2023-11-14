4a48a4b2ae45cbb0235d7868cca630db2a39df6f
LakshmiKMVinayaBhakshi <lakshmikm.h9@outlook.com>
2023-11-14 19:00:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
+import './Calculator.css'
 
+const ACTIONS = {
+  ADD_DIGIT:'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT:'delete-digit',
+  EVALUATE : 'evaluate'
+
+}
+function reducer(state,{type,payload}){
+switch(type){
+  case  ACTIONS.ADD_DIGIT:
+    return{
+      ...state,
+      currentOperand: `$(currentOperand || "")$(payload.digit)`
+    }
+}
+}
 function App() {
+  // const [state,dispatch] = useReducer(reducer,{})  => is the syntax of useReducer hook 
+  const[currentOperand, previousOperand, operation] = useReducer(reducer,{})
+
+  dispatch({type:ACTIONS.ADD_DIGIT, paload:{digit:1}})
   return (
-    <div>hello </div>
+    <div className='calculator-grid'>
+    <div className='output'>
+    <div className='previous-operand'>{previousOperand}{operation}</div>
+    <div className='current-operand'>{currentOperand}</div>
+    </div> 
+    <button className='span-two'>AC</button>
+    <button>DEL</button>
+    <button>÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>x</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button className='span-two'>=</button>
+
+
+    </div> 
   );
 }
 
